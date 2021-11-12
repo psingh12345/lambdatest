06a3e9dc41bbdeab5e8466e80c68d176c0c5244d
Prabhjot Singh <er.prabhjotsingh89@gmail.com>
2021-11-12 18:41:08 +0530

    test push

--- lambda_function.py
+++ lambda_function.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 def lambda_handler(event,context):
     print('pandas deployed and executed........')
+    lambda_prabh("hello i am prabh")
+
+def lambda_prabh(a):
+    print(a)
